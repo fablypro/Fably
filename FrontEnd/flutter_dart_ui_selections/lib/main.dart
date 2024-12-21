0bd52c2b4de530167cd7e2160bb94801917892fd
AvishaiJASAG <avishai.20232615@iit.ac.lk>
2024-12-21 19:03:24 +0530

    changed coments

--- FrontEnd/flutter_dart_ui_selections/lib/main.dart
+++ FrontEnd/flutter_dart_ui_selections/lib/main.dart
@@ -47,8 +47,7 @@ class MyHomePage extends StatefulWidget {
 
   // This class is the configuration for the state. It holds the values (in this
   // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  // used by the build method of the State.
 
   final String title;
 
@@ -74,7 +73,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
-    //
+
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
