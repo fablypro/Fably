b7462bec5f28555d02d46dcb00910f9b1f90f82c
AvishaiJASAG <avishai.20232615@iit.ac.lk>
2025-01-20 16:40:13 +0530

    edited dart files.

---
+++ flutter_dart_ui_selections/lib/BeltsWidget.dart
@@ -0,0 +1,45 @@
+// importing all material from package:flutter/material.dart.
+import 'package:flutter/material.dart';
+
+// visuals of Suggestions page.
+class AccessorySuggestions extends StatefulWidget {
+  // creating the visuals of the Suggestions page title.
+  const AccessorySuggestions({super.key, required this.title});
+
+  // declare the title of the Suggestions page.
+  final String title;
+
+  @override
+  State<AccessorySuggestions> createState() => _AccessorySuggestions();
+}
+
+// classes for Belts screen.
+class BeltsWidget extends StatelessWidget {
+  const BeltsWidget({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // widget title.
+        title: const Text('Belts'),
+        actions: const [
+          // padding for the selections screen page.
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+          ),
+        ],
+      ),
+      body: Center(
+        // vertically arranges the buttons.
+        child: Column(
+          // centering the buttons vertically.
+          mainAxisAlignment: MainAxisAlignment.center,
+          // selection buttons.
+          children: <Widget>[
+          ],
+        ),
+      ),
+    );
+  }
+}
