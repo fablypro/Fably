0969bc09de2b60d59fd4e97b068badab64d9aa73
AvishaiJASAG <avishai.20232615@iit.ac.lk>
2025-02-03 19:08:58 +0530

    added cart class file and dependencies

--- flutter_ui_selections/lib/cart/cart.dart
+++ flutter_ui_selections/lib/cart/cart.dart
@@ -4,35 +4,39 @@ import 'package:flutter/material.dart';
 
 // cart class for the cart objects.
 class Cart {
-  final int? id;
+  late final int? id;
   final String? productId;
   final String? productName;
   final int? productPrice;
-  final ;
-  final ;
+  final String? unitTag;
   final String? image;
 
   //cart constructor to initialize values.
   Cart(
-    {
-      required this.id,
+    {required this.id,
       required this.productId,
       required this.productName,
       required this.productPrice,
+      required this.unitTag,
       required this.image,
-    }
-  );
+    });
 
   Cart.fromMap(Map<dynamic, dynamic> data)
       : id = data['id'],
         productId = data['productId'],
-  productName = data['productName'];
+        productName = data['productName'],
+        productPrice = data['productPrice'],
+        unitTag = data['unitTag'],
+        image = data['image'];
 
   Map<String, dynamic> toMap() {
     return {
       'id': id,
       'productId': productId,
       'productName': productName,
+      'productPrice': productPrice,
+      'unitTag': unitTag,
+      'image': image,
     };
   }
 }
