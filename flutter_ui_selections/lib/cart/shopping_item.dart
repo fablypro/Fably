2c1028def7bf9d19ae6d37f0c7415460d078690a
AvishaiJASAG <avishai.20232615@iit.ac.lk>
2025-02-04 19:58:32 +0530

    added cart class file and dependencies

---
+++ flutter_ui_selections/lib/cart/shopping_item.dart
@@ -0,0 +1,24 @@
+// cart class for the cart objects.
+class Item {
+  final String prodName;
+  final int prodPrice;
+  final String unit;
+  final String img;
+
+  //cart constructor to initialize values.
+  Item(
+      {required this.prodName,
+        required this.prodPrice,
+        required this.unit,
+        required this.img,
+      });
+
+  Map toJson() {
+    return {
+      this.prodName,
+      this.prodPrice,
+      this.unit,
+      his.img,
+    }
+  }
+}
