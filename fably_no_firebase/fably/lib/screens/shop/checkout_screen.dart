9add9b0b93f779612e0856a295be18714a538631
Dashin sanjana <dashinsanjana124@gmail.com>
2025-03-08 08:12:53 +0530

    Update checkout_screen.dart

--- fably_no_firebase/fably/lib/screens/shop/checkout_screen.dart
+++ fably_no_firebase/fably/lib/screens/shop/checkout_screen.dart
@@ -2,3 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+
+class CheckoutScreen extends StatefulWidget {
+  const CheckoutScreen({super.key});
+
+  @override
+  _CheckoutScreenState createState() => _CheckoutScreenState();
+}
+
+class _CheckoutScreenState extends State<CheckoutScreen> {
+  bool processingCheckout = false;
+
+  void _showMessage(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  Future<void> makePayment() async {
+    setState(() {
+      processingCheckout = true;
+    });
