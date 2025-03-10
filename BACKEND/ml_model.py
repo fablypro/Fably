abd20e9674b1b4cbdfb63b6d86bf0bdceeff8179
AvishaiJASAG <avishai.20232615@iit.ac.lk>
2025-03-11 01:29:46 +0530

    update ml_model py
    update ml_model py

---
+++ BACKEND/ml_model.py
@@ -0,0 +1,14 @@
+import os
+import cv2 as c
+import numpy as np
+from dotenv import load_dotenv # type: ignore
+import tensorflow as tf # type: ignore
+from tensorflow.keras.models import load_model as keras_load_model # type: ignore
+
+
+
+
+
+
+
+
