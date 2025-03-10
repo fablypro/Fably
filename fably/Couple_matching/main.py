f507dad79094d4fc46a23bca92e22aacd3579097
Yehan001 <yehan.20232373@iit.ac.lk>
2025-03-11 00:55:08 +0530

    Update main.py
    updated clothing items

--- fably/Couple_matching/main.py
+++ fably/Couple_matching/main.py
@@ -3,7 +3,23 @@ import random
 class OutfitMatcher:
     def __init__(self):
         self.clothing_items = {
-            
+            "men":{
+                "casual":{
+                    "top":["T-shirt","polo shirt","Casual shirt"],
+                    "bottom":["jeans","chinos","Shorts"],
+                    "shoes":["Sneakers","Loafers"]
+                },
+                "formal":{
+                    "top":["Dress shirt","Blazer"],
+                    "bottom":["Dress pants","Suit pants"],
+                    "shoes":["Oxfords","Derby shoes"]
+                },
+                "sporty":{
+                    "top":["Tank top","sporty jersey"],
+                    "bottom":["Joggers","Shorts"],
+                    "shoes":["Running shoes","Trainers"]
+                }
+            }
 
             
         }
