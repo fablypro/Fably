9c35c46a89addb9f7686b5fde39d6c2fddfb9c1c
AvishaiJASAG <avishai.20232615@iit.ac.lk>
2025-03-11 01:00:21 +0530

    update accessory_matching.py
    update accessory_matching.py

--- BACKEND/accessory_matching.py
+++ BACKEND/accessory_matching.py
@@ -6,3 +6,17 @@ from sklearn import preprocessing # type: ignore
 from dotenv import load_dotenv # type: ignore
 import tensorflow as tf # type: ignore
 
+from werkzeug.utils import secure_filename # type: ignore
+
+# importing form machine learning model.
+from ml_model import (
+    matching_colors_between_outfits_and_accessories, 
+    extract_main_colors, 
+    predict_accessory, 
+    predict_outfit, 
+    load_model_via_pretrained_CNN, 
+    #load_model
+)
+
+
+
