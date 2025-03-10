f93005e3119e499a0757828e8d89fcd3338a1c03
AvishaiJASAG <avishai.20232615@iit.ac.lk>
2025-03-11 01:30:35 +0530

    update model py
    update model py

--- BACKEND/ml_model.py
+++ BACKEND/ml_model.py
@@ -5,7 +5,13 @@ from dotenv import load_dotenv # type: ignore
 import tensorflow as tf # type: ignore
 from tensorflow.keras.models import load_model as keras_load_model # type: ignore
 
+# adding AI learning component.
+from tensorflow.keras.applications import ResNet50 # type: ignore
 
+from tensorflow.keras.preprocessing import image # type: ignore
+from tensorflow.keras.applications.resnet50 import preprocess_input # type: ignore
+
+from sklearn.cluster import KMeans # type: ignore
 
 
 
