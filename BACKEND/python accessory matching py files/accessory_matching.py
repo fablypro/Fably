e96b76eb1f11ae3ba8f0d6dc226f1993d063f5fd
AvishaiJASAG <avishai.20232615@iit.ac.lk>
2025-03-12 08:11:01 +0530

    update accesosry matinch py
    update accesosry matinch py

--- BACKEND/python accessory matching py files/accessory_matching.py
+++ BACKEND/python accessory matching py files/accessory_matching.py
@@ -130,13 +130,9 @@ def upload_file():
 
             # sending predictions.
             return jsonify({
-                "pretrained accessory match found": bool(pretrained_accessory_match_found),
-                "pretrained accessory confidence": float(pretrained_accessory_confidence), 
-                "pretrained accessory prediction": int(pretrained_accessory_prediction),
+                "pretrained accessory match found": bool(pretrained_accessory_match_found), "pretrained accessory confidence": float(pretrained_accessory_confidence), "pretrained accessory prediction": int(pretrained_accessory_prediction),  
                 
-                "pretrained outfit match found": bool(pretrained_outfit_match_found),
-                "pretrained outfit confidence": float(pretrained_outfit_confidence), 
-                "pretrained outfit prediction": int(pretrained_outfit_prediction),
+                "pretrained outfit match found": bool(pretrained_outfit_match_found), "pretrained outfit confidence": float(pretrained_outfit_confidence), "pretrained outfit prediction": int(pretrained_outfit_prediction),        
                 
                 "pretrained match found message": "Pretrained Match Found!" 
                 if (pretrained_accessory_match_found 
