99b9ec1dbaba1cdefa9bf5e78b9e0d8d71deb1b1
AvishaiJASAG <avishai.20232615@iit.ac.lk>
2025-03-19 18:52:55 +0530

    updating dart codee
    updating dart codee

---
+++ FRONTEND/fably/lib/accessory matching/accessory_matching.dart
@@ -0,0 +1,33 @@
+import 'package: flutter/material.dart';
+import 'dart: convert';
+import 'dart: io';
+import 'package: image_picker/image_picker.dart';
+import 'package: http/http.dart' as http;
+
+void main() {
+    runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+        return MaterialApp(
+            title: 'Accessory Matcher',
+            theme: ThemeData(
+                primarySwatch: Colors.blue,
+            ),
+            home: AccessoryMatcher(),
+        );
+    }
+}
+
+class AccessoryMatcher extends StatefulWidget {
+    @override
+    _AccessoryMatcherState createState() => _AccessoryMatcherState();
+}
+
+
+
+
+
+
