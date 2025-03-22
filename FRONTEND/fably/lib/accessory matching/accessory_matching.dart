761df8bdf2726e17773b9ad162510a85a4b3d5b3
AvishaiJASAG <avishai.20232615@iit.ac.lk>
2025-03-22 10:39:48 +0530

    updating stuff
    updating dependencies, my flutter frontend and my pytohn code

--- FRONTEND/fably/lib/accessory matching/accessory_matching.dart
+++ FRONTEND/fably/lib/accessory matching/accessory_matching.dart
@@ -1,8 +1,12 @@
-import 'package: flutter/material.dart';
-import 'dart: convert';
-import 'dart: io';
-import 'package: image_picker/image_picker.dart';
-import 'package: http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
+import 'package:loading_overlay/loading_overlay.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:path/path.dart' as path;
 
 void main() {
     runApp(MyApp());
@@ -27,7 +31,7 @@ class AccessoryMatcher extends StatefulWidget {
 }
 
 class _AccessoryMatcherState extends State<AccessoryMatcher> {
-    ImagePicker _picker = ImagePicker();
+    final ImagePicker _picker = ImagePicker();
     Map<String, XFile?> _images = {
         'belts': null,
         'chains': null,
@@ -44,7 +48,6 @@ class _AccessoryMatcherState extends State<AccessoryMatcher> {
     };
 
     Map<String, dynamic> _results = {};
-
     bool _isLoading = false;
 
     Future<void> _pickImage(String type) async {
@@ -62,9 +65,23 @@ class _AccessoryMatcherState extends State<AccessoryMatcher> {
             _results = {};
         });
 
-        var uri = Uri.parse('uri');
+        final var uri = Uri.parse('uri');
 
-        var request = http.MultipartRequest('POST', uri);
+        try {
+            var request = http.MultipartRequest('POST', uri);
+        }
+    }
+
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            appBar: AppBar(
+                title: Text('Accessory Matching'),
+                backgroundColor: Colors.black,
+                actions: [
+                ],
+            )
+            );
     }
 }
 
