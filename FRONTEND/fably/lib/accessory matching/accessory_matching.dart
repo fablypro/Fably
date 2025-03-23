274bca55a421ac6f0f9249a33c5814b0c394bc05
AvishaiJASAG <avishai.20232615@iit.ac.lk>
2025-03-23 18:31:33 +0530

    update code syntax
    update code syntax

--- FRONTEND/fably/lib/accessory matching/accessory_matching.dart
+++ FRONTEND/fably/lib/accessory matching/accessory_matching.dart
@@ -3,16 +3,15 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:dio/dio.dart' as dio;
 import 'package:image_picker/image_picker.dart';
-import 'package:http/http.dart' as http;
 import 'package:loading_overlay/loading_overlay.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:mime/mime.dart';
 
 void main() {
-    runApp(MyApp());
+    runApp(AccessoryMatchApp());
 }
 
-class MyApp extends StatelessWidget {
+class AccessoryMatchApp extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
         return MaterialApp(
@@ -40,27 +39,11 @@ class _AccessoryMatcherState extends State<AccessoryMatcher> {
     Map<String, dynamic> _results = {};
     bool _isLoading = false;
 
-    final List<String> _accessoryTypes = ['belts', 'chains',
-                                    'glasses', 'gloves',
-                                    'handbags', 'hats',
-                                    'rings', 'shoes',
-                                    'socks', 'watches'];
+    final List<String> _accessoryTypes = ['belts', 'chains', 'glasses', 'gloves', 'handbags', 'hats', 'rings', 'shoes', 'socks', 'watches'];
 
-    final List<String> _ouftitCatgories = ['activewear', 'bohemian',
-                                    'casual', 'eveningwear',
-                                    'formal', 'indie',
-                                    'knitwear', 'loungewear',
-                                    'retro', 'romantic',
-                                    'smartcasual', 'sporty', 'vintage'];
+    final List<String> _ouftitCatgories = ['activewear', 'bohemian', 'casual', 'eveningwear', 'formal', 'indie', 'knitwear', 'loungewear', 'retro', 'romantic', 'smartcasual', 'sporty', 'vintage'];
 
-    final List<String> _colorsList = ['Amber', 'Black', 'Blue', 'Emerald',
-                                    'Gold', 'Green', 'Grey', 'Indigo',       
-                                    'Jade', 'Lemon', 'Lilac', 'Lime',
-                                    'Midnight Blue', 'Mint Green', 'Navy Blue', 'Olive',
-                                    'Orange', 'Peach', 'Pink', 'Platinum',
-                                    'Plum', 'Purple', 'Red', 'Rose',
-                                    'Ruby', 'Sapphire', 'Scarlet', 'Silver',
-                                    'Turquoise', 'Ultramarine', 'Violet', 'White', 'Yellow', 'Zucchini'];
+    final List<String> _colorsList = ['Amber', 'Black', 'Blue', 'Emerald', 'Gold', 'Green', 'Grey', 'Indigo', 'Jade', 'Lemon', 'Lilac', 'Lime', 'Midnight Blue', 'Mint Green', 'Navy Blue', 'Olive', 'Orange', 'Peach', 'Pink', 'Platinum', 'Plum', 'Purple', 'Red', 'Rose', 'Ruby', 'Sapphire', 'Scarlet', 'Silver', 'Turquoise', 'Ultramarine', 'Violet', 'White', 'Yellow', 'Zucchini'];
     
 
     @override
@@ -82,7 +65,7 @@ class _AccessoryMatcherState extends State<AccessoryMatcher> {
             setState(() {
                 _imageFiles[type] = image;
             });
-        };
+        }
     }
 
     Future<void> _uploadImages() async {
@@ -100,15 +83,11 @@ class _AccessoryMatcherState extends State<AccessoryMatcher> {
                 }
             });
             
-            _accessoryColors.forEach((key, value) {
-                formData.fields.add(MapEntry('accessory_${key}_color', value));
-            });
-            _outfitTypes.forEach((key, value) {
-                formData.fields.add(MapEntry('outfit_${key}_type', value));
-            });
-            _outfitColors.forEach((key, value) {
-                formData.fields.add(MapEntry('outfit_${key}_color', value));
-            });
+            _accessoryColors.forEach((key, value) { formData.fields.add(MapEntry('accessory_${key}_color', value)); });
+
+            _outfitTypes.forEach((key, value) { formData.fields.add(MapEntry('outfit_${key}_type', value)); });
+
+            _outfitColors.forEach((key, value) { formData.fields.add(MapEntry('outfit_${key}_color', value)); });
 
             var response = await dio.Dio().post('Fably/BACKEND/python accessory matching py files/static/images', data: formData);
             setState(() {
@@ -134,11 +113,9 @@ class _AccessoryMatcherState extends State<AccessoryMatcher> {
                         padding: EdgeInsets.all(16.0),
                         child: Column(
                         children: [
-                            Text('Accessory Matching With Outfits',
-                                style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold)),
+                            Text('Accessory Matching With Outfits', style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold)),
                             SizedBox(height: 20),
-                            Text('Accessories',
-                                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500)),
+                            Text('Accessories', style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500)),
                             ..._accessoryTypes.map((type) {
                                 Column(
                                     children: [
@@ -156,8 +133,7 @@ class _AccessoryMatcherState extends State<AccessoryMatcher> {
                                                     _accessoryColors[type] = newValue!;
                                                 });
                                             },
-                                            decoration: InputDecoration(
-                                                labelText: "Choose ${type.toUpperCase()} Color"),
+                                            decoration: InputDecoration(labelText: "Choose ${type.toUpperCase()} Color"),
                                         ),
                                         SizedBox(height: 10),
                                         ElevatedButton(
@@ -166,49 +142,47 @@ class _AccessoryMatcherState extends State<AccessoryMatcher> {
                                         ),
                                         if (_imageFiles[type] != null) Image.file(File(_imageFiles[type]!.path), height: 100),
                                         SizedBox(height: 20),
-                                    ],
-                                );
-                                Text('Outfits',
-                                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500));
-                                for (var type in _ouftitCatgories) {
-                                    Column(
-                                        children: [
-                                            DropdownButtonFormField(
-                                                value: _outfitTypes[type],
-                                                items: _ouftitCatgories.map((String value) {
-                                                    return DropdownMenuItem<String>(
-                                                        value: value,
-                                                        child: Text(value),
-                                                    );
-                                                }).toList(), 
-                                                onChanged: (String? newValue) {
-                                                    setState(() {
-                                                        _outfitTypes[type] = newValue!;
-                                                    });
-                                                },
-                                                decoration: InputDecoration(
-                                                    labelText: "Choose Outfit Style"),
-                                            ),
-                                            SizedBox(height: 10),
-                                            ElevatedButton(
-                                                onPressed: () => _pickImage(type), 
-                                                child: Text('Upload ${type.toUpperCase()} Image'),
-                                            ),
-                                            if (_imageFiles[type] != null) Image.file(File(_imageFiles[type]!.path), height: 100),
-                                            SizedBox(height: 20),
                                         ],
                                     );
-                                    ElevatedButton(
-                                        onPressed: _uploadImages,
-                                        child: Column(
+                                    Text('Outfits', style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500));
+                                    for (var type in _ouftitCatgories) {
+                                        Column(
                                             children: [
-                                                Text('Match Images'),
-                                                if (_results.isNotEmpty) Text(jsonEncode(_results), style: TextStyle(fontStyle: 16.0)),
+                                                DropdownButtonFormField(
+                                                    value: _outfitTypes[type],
+                                                    items: _ouftitCatgories.map((String value) {
+                                                        return DropdownMenuItem<String>(
+                                                            value: value,
+                                                            child: Text(value),
+                                                        );
+                                                    }).toList(), 
+                                                    onChanged: (String? newValue) {
+                                                        setState(() {
+                                                            _outfitTypes[type] = newValue!;
+                                                        });
+                                                    },
+                                                    decoration: InputDecoration(labelText: "Choose Outfit Style"),
+                                                ),
+                                                SizedBox(height: 10),
+                                                ElevatedButton(
+                                                    onPressed: () => _pickImage(type), 
+                                                    child: Text('Upload ${type.toUpperCase()} Image'),
+                                                ),
+                                                if (_imageFiles[type] != null) Image.file(File(_imageFiles[type]!.path), height: 100),
+                                                SizedBox(height: 20),
                                             ],
-                                        ),
-                                );
-                            };
-                            },
+                                        );
+                                        ElevatedButton(
+                                            onPressed: _uploadImages,
+                                            child: Column(
+                                                children: [
+                                                    Text('Match Images'),
+                                                    if (_results.isNotEmpty) Text(jsonEncode(_results), style: TextStyle(fontSize: 16.0)),
+                                                ],
+                                            ),
+                                        );
+                                    }
+                                },
                             ),
                         ],
                     ),
