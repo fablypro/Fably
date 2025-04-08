86ef3916ad973ca207b135a6d075d5e735e1a891
Yehan001 <yehan.20232373@iit.ac.lk>
2025-04-08 11:20:14 +0530

    kkk
    kkkkk

---
+++ fably/lib/viva.py
@@ -0,0 +1,5 @@
+print("This is for my viva")
+
+ender_input = "men" 
+matching_outfit = matcher.get_matching_outfit(gender_input, style_choice)
+print(f"Matching outfit for {gender_input}:", matching_outfit)
