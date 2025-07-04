833fe51e50ad0dd46d69092af123c3f9822d747c
Yehan001 <yehan.20232373@iit.ac.lk>
2025-07-04 16:15:34 +0530

    created trending class

---
+++ FRONTEND/fably/lib/screens/shop/Trending.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:async';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'checkout_screen.dart';
+import '../auth/login.dart';
+import '../shop/product.dart';
+import '../../utils/requests.dart';
+import '../../utils/prefs.dart';
